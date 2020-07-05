0b2e27e867260051780095d064003e07212222a9
batiscuff <56800387+batiscuff@users.noreply.github.com>
2020-07-06 02:42:06 +0300

    Update spymer.py

--- spymer.py
+++ spymer.py
@@ -160,8 +160,6 @@ def start():
         except:
             print(fail + "Kasta не отправлен" + Style.RESET_ALL)
         try:
-            
-        try:
             requests.post(
                 "https://izi.ua/api/auth/register",
                 json={
